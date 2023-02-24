252e2e70b2f0e0451d8b6598cb8f02ca893189ac
phoebus-84 <ennio@dyne.org>
2023-02-24 01:40:46 +0100

    feat: add zenroom to verify signature

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
 import cors from "cors";
 import express from "express";
 import { handleProjectCreation } from "./handlers/createProjectHandler.js";
+import {zenroom_exec} from "zenroom";
+import {verifyGraphql} from "./zenflows-crypto/verifyGraphql.js";
 
 const app = express();
 app.use(cors());
 app.use(express.text());
+app.use((req, res, next) => {
+  const zenflowsId = req.headers["zenflows-id"];
+  const zenflowsSign = req.headers["zenflows-sign"];
+  if (!zenflowsId || !zenflowsSign) {
+    res.status(401);
+    res.send("Unauthorized");
+  } 
+  const zenroomVariables = {
+    gql: req.body,
+    zenflowsSign: zenflowsSign,
+  };
+  const check = zenroom_exec(verifyGraphql, zenroomVariables);
+  if (check !== "1") {
+    res.status(401);
+    res.send("Unauthorized");
+  }
+  next();
+});
 
 app.post("/create/project", async (req, res) => {
   const variables = req.body;
-  console.log(variables);
   try {
     const response = await handleProjectCreation(JSON.parse(variables));
     res.send(response);
   } catch (error) {
     console.error(error);
-    res.sendStatus(500);
+    res.status(500);
+    res.send(error);
   }
 });
 
